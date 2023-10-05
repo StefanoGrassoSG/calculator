f7c4ced55a9c660ee00c154422c11f541926bbd0
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-10-05 17:07:48 +0200

    add all numbers

--- calculator/listenerButtons.js
+++ calculator/listenerButtons.js
@@ -6,7 +6,11 @@ if (value === '') {
 
 
 document.getElementById('1').addEventListener('click', function() {
-    value = value + '1';
+    if (value === '0') {
+        value = '1';
+    } else {
+        value += '1';
+    }
     console.log(value)
     const div = document.createElement('div');
     div.textContent = "1";
@@ -16,7 +20,11 @@ document.getElementById('1').addEventListener('click', function() {
 
   
 document.getElementById('2').addEventListener('click', function() {
-    value = value + '2';
+    if (value === '0') {
+        value = '2';
+    } else {
+        value += '2';
+    }
     console.log(value)
     const div = document.createElement('div');
     div.textContent = "2";
@@ -24,6 +32,96 @@ document.getElementById('2').addEventListener('click', function() {
     update()
   });
 
+  document.getElementById('3').addEventListener('click', function() {
+    if (value === '0') {
+        value = '3';
+    } else {
+        value += '3';
+    }
+    console.log(value)
+    const div = document.createElement('div');
+    div.textContent = "3";
+    document.getElementById('calc').appendChild(div);
+    update()
+  });
+
+  document.getElementById('4').addEventListener('click', function() {
+    if (value === '0') {
+        value = '4';
+    } else {
+        value += '4';
+    }
+    console.log(value)
+    const div = document.createElement('div');
+    div.textContent = "4";
+    document.getElementById('calc').appendChild(div);
+    update()
+  });
+
+  document.getElementById('5').addEventListener('click', function() {
+    if (value === '0') {
+        value = '5';
+    } else {
+        value += '5';
+    }
+    console.log(value)
+    const div = document.createElement('div');
+    div.textContent = "5";
+    document.getElementById('calc').appendChild(div);
+    update()
+  });
+
+  document.getElementById('6').addEventListener('click', function() {
+    if (value === '0') {
+        value = '6';
+    } else {
+        value += '6';
+    }
+    console.log(value)
+    const div = document.createElement('div');
+    div.textContent = "6";
+    document.getElementById('calc').appendChild(div);
+    update()
+  });
+
+  document.getElementById('7').addEventListener('click', function() {
+    if (value === '0') {
+        value = '7';
+    } else {
+        value += '7';
+    }
+    console.log(value)
+    const div = document.createElement('div');
+    div.textContent = "7";
+    document.getElementById('calc').appendChild(div);
+    update()
+  });
+
+  document.getElementById('8').addEventListener('click', function() {
+    if (value === '0') {
+        value = '8';
+    } else {
+        value += '8';
+    }
+    console.log(value)
+    const div = document.createElement('div');
+    div.textContent = "8";
+    document.getElementById('calc').appendChild(div);
+    update()
+  });
+
+  document.getElementById('9').addEventListener('click', function() {
+    if (value === '0') {
+        value = '9';
+    } else {
+        value += '9';
+    }
+    console.log(value)
+    const div = document.createElement('div');
+    div.textContent = "9";
+    document.getElementById('calc').appendChild(div);
+    update()
+  });
 
 
   document.getElementById('C').addEventListener('click', function() {
